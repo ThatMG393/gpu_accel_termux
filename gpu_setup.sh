2f83762082c987ed5217105b2505d9d8931c80ef
ThatMG393 <thatmg393@gmail.com>
2023-02-16 22:00:12 +0800

    Add a whole ton of fixes, features, and also bugs

--- gpu_setup.sh
+++ gpu_setup.sh
@@ -5,7 +5,12 @@
 # AUTOMATED BY Thundersnow, ThatMG393
 # PATCHES MADE BY Thundersnow
 
+set -e
 clear
+
+# Possible values can only be 'enable', 'fix', and 'disable'
+# Putting another values will just disable xf86bigfont
+USE_XF86BF="fix"
 
 # Yoink from UDroid
 DIE() { echo -e "${@}"; exit 1 ;:; }
@@ -20,21 +25,37 @@ Nnw_INFO() { echo -n -e "\e[32m${*}\e[0m";:; }
 
 TITLE() { echo -e "\e[100m${*}\e[0m";:; }
 
+SIG_HANDLER() {
+	clear
+	DIE "Immediately cancelling as the user requested..."
+}
+
+trap 'SIG_HANDLER' SIGKILL SIGINT
+
+if [ -d "/usr" ]; then DIE "Building inside a proot is not supported!"; fi
+
+DEPENDENCIES="vulkaninfo"
+
+INFO "Checking for '$DEPENDENCIES'..."
+for DEPENDENCY in $DEPENDENCIES; do
+	if [[ ! -n $(command -v $DEPENDENCY) || $( $DEPENDENCY |& grep "(No such file or directory|Command not found)" | wc -l ) == 1 ]]; then
+		INFO "Downloading '$DEPENDENCY'..."
+		pkg install $DEPENDENCY -y &> /dev/null && INFO "Success!" || DIE "Failed!"
+	else
+		INFO "'$DEPENDENCY' already installed!"
+	fi
+done
+
+# Utils
+RM_SILENT() { rm -rf "${*}" &> /dev/null ;:; }
+
 echo ""
 TITLE " █▀▀ █▀█ █░█     ▄▀█ █▀▀ █▀▀ █▀▀ █░   ░ ▄▀ █░█ █▀ █ █▄░█ █▀▀    ▀█ █ █▄░█ █▄▀ ▀▄  "
 TITLE " █▄█ █▀▀ █▄█     █▀█ █▄▄ █▄▄ ██▄ █▄▄    ▀▄ █▄█ ▄█ █ █░▀█ █▄█    █▄ █ █░▀█ █░█ ▄▀  "
 echo ""
 GWARN "Activating GPU Acceleration (via Zink)"
 
-INFO "Checking for 'vulkaninfo'..."
-if [[ ! -n $(command -v vulkaninfo) || $( vulkaninfo |& grep "(No such file or directory|Command not found)" | wc -l ) == 1 ]]; then
-	INFO "Downloading 'vulkaninfo'..."
-	pkg install vulkan-tools -y &> /dev/null && INFO "Success!" || DIE "Failed!"
-else
-	INFO "'vulkaninfo' already installed!"
-fi
-
-INFO "Checking for requirments..."
+INFO "Checking for requirements..."
 
 #GPU_VULKAN_SUPPORT=$( getprop | grep "ro.hardware.vulkan" | grep -Po "\[[a-z]*\]" )
 #echo -n "Is $GPU_VULKAN_SUPPORT supported?"
@@ -85,23 +106,22 @@ mkdir -p $MAIN_FOLDER
 
 TMP_FOLDER="$MAIN_FOLDER/tmp"
 
-# FIXME: Auto-extract patches and diff files.
 MESA_PATCH_FILE="$MAIN_FOLDER/mesa20230212.patch"
 XSERVER_PATCH_FILE="$MAIN_FOLDER/xserver.patch"
 VIRGL_DIFF_FILE="$MAIN_FOLDER/virglrenderer.diff"
 
 PATCHES_TAR_GZ="$MAIN_FOLDER/patches.tar.gz"
 
-nw_INFO "Checking for patches and diff files..."
+INFO "Checking for patches and diff files..."
 
 [[ ! -f $MESA_PATCH_FILE || ! -f $XSERVER_PATCH_FILE || ! -f $VIRGL_DIFF_FILE ]] && {
 	INFO "Fetching & Extracting 'patches.tar.gz'"
-	rm $MESA_PATCH_FILE $XSERVER_PATCH_FILE $VIRGL_DIFF_FILE &> /dev/null
+	RM_SILENT $MESA_PATCH_FILE $XSERVER_PATCH_FILE $VIRGL_DIFF_FILE &> /dev/null
 	
 	cd $MAIN_FOLDER
 	# [ $( gzip -t $PATCHES_TAR_GZ && $? ) != 0 ] && {
 	[ ! -f $PATCHES_TAR_GZ ] && {
-		rm $PATCHES_TAR_GZ &> /dev/null # Sanity check
+		RM_SILENT $PATCHES_TAR_GZ &> /dev/null # Sanity check
 		wget https://raw.githubusercontent.com/ThatMG393/gpu_accel_termux/master/patches.tar.gz &> /dev/null && {
 			nw_INFO "Success! (1/2)"
 		} || {
@@ -110,7 +130,7 @@ nw_INFO "Checking for patches and diff files..."
 	}
 	
 	tar -xf $PATCHES_TAR_GZ &> /dev/null && {
-		nw_INFO "Success! (2/2)"
+		nw_INFO "\33[2K\rSuccess! (2/2)"
 	} || {
 		DIE "Failed to extract 'patches.tar.gz'. Is 'wget' and 'tar' installed? Try re-running the script."
 	}
@@ -127,22 +147,22 @@ clear
 TITLE "AUTO INSTALLATION STARTED"
 
 pkg install -y x11-repo -y
-pkg install -y clang lld binutils cmake autoconf automake libtool '*ndk*' make python git libandroid-shmem-static 'vulkan*' ninja llvm bison flex libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama -y
+pkg install -y clang lld binutils cmake -s  autoconf automake -s  libtool '*ndk*' make -s  python git libandroid-shmem-static 'vulkan*' ninja llvm bison flex libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama -y
 pip install meson mako
 
-mkdir -p $TMP_FOLDER
-cd $TMP_FOLDER
-
 [ -d $TMP_FOLDER ] && {
-	INFO "The folder $TMP_FOLDER already exists do you want to re-clone the repositories? (y|n)"
+	INFO "The repositories folder already exists do you want to re-clone the repositories? (y|n)"
 	
 	read -p "" ANSWER
 	
 	case $ANSWER in
-		y | yes ) rm -rf $TMP_FOLDER ;;
-		n | no  ) INFO "Skipping..." ;;
+		y | Y | yes ) RM_SILENT $TMP_FOLDER ;;
+		n | N | no  ) INFO "Skipping..." ;;
 	esac
 }
+
+mkdir -p $TMP_FOLDER
+cd $TMP_FOLDER
 
 LD_PRELOAD='' git clone --depth 1 https://gitlab.freedesktop.org/mesa/mesa.git
 LD_PRELOAD='' git clone --depth 1 https://gitlab.freedesktop.org/virgl/virglrenderer.git
@@ -155,17 +175,27 @@ LD_PRELOAD='' git clone --depth 1 -b 0.3 https://github.com/dottedmag/libsha1.gi
 LD_PRELOAD='' git clone --depth 1 -b xorg-server-1.20.14 https://gitlab.freedesktop.org/xorg/xserver.git
 
 #compile libxshmfence
+clear
+TITLE "Compiling libxshmfence... (1/8)"
+echo ""
+
 cd $TMP_FOLDER/libxshmfence
 sed -i s/values.h/limits.h/ ./src/xshmfence_futex.h
 
-rm $PREFIX/lib/libxshmfence*
+RM_SILENT $PREFIX/lib/libxshmfence*
 
 ./autogen.sh --prefix=$PREFIX --with-shared-memory-dir=$TMPDIR
-make -j8 install CPPFLAGS=-DMAXINT=INT_MAX
+make -s -j8 install CPPFLAGS=-DMAXINT=INT_MAX
 
 #compile mesa
+clear
+TITLE "Compiling & Patching mesa... (2/8)"
+echo ""
+
 cd $TMP_FOLDER/mesa
-[ ! -f $MESA_PATCH_FILE ] && DIE "Mesa patch file not found! Try re-running the script..."
+[ ! -f $MESA_PATCH_FILE ] && {
+	DIE "Mesa patch file not found! Try re-running the script..."
+}
 git apply $MESA_PATCH_FILE
 
 mkdir b
@@ -173,15 +203,19 @@ cd b
 
 LDFLAGS='-l:libandroid-shmem.a -llog' meson .. -Dprefix=$PREFIX -Dplatforms=x11 -Dgbm=enabled -Dgallium-drivers=zink,swrast -Dllvm=enabled -Dvulkan-drivers='' -Dcpp_rtti=false -Dc_args=-Wno-error=incompatible-function-pointer-types -Dbuildtype=release
 
-rm $PREFIX/lib/libglapi.so*
-rm $PREFIX/lib/libGL.so*
-rm $PREFIX/lib/libGLES*
-rm $PREFIX/lib/libEGL*
-rm $PREFIX/lib/libgbm*
+RM_SILENT $PREFIX/lib/libglapi.so*
+RM_SILENT $PREFIX/lib/libGL.so*
+RM_SILENT $PREFIX/lib/libGLES*
+RM_SILENT $PREFIX/lib/libEGL*
+RM_SILENT $PREFIX/lib/libgbm*
 
-ninja install
+ninja install -d stats
 
 #compile libepoxy
+clear
+TITLE "Compiling libepoxy... (3/8)"
+echo ""
+
 cd $TMP_FOLDER/libepoxy
 
 mkdir b
@@ -189,25 +223,36 @@ cd b
 
 meson -Dprefix=$PREFIX -Dbuildtype=release -Dglx=yes -Degl=yes -Dtests=false -Dc_args=-U__ANDROID__ ..
 
-rm $PREFIX/lib/libepoxy*
+RM_SILENT $PREFIX/lib/libepoxy*
 
-ninja install
+ninja install -d stats
 
 #compile virglrenderer
+clear
+TITLE "Compiling & Patching virglrenderer... (4/8)"
+echo ""
+
 cd $TMP_FOLDER/virglrenderer
 
-[ ! -f $VIRGL_PATCH_FILE ] && DIE "VirGL diff file not found! Try re-running the script..."
-git checkout -f master
-git apply $VIRGL_PATCH_FILE
+[ ! -f $VIRGL_PATCH_FILE ] && {
+	DIE "VirGL diff file not found! Try re-running the script..."
+}
+# git checkout -f master
+git apply $VIRGL_DIFF_FILE
 
+mkdir b
 cd b
 
-rm $PREFIX/lib/libvirglrenderer*
+RM_SILENT $PREFIX/lib/libvirglrenderer*
 
-ninja install
+ninja install -d stats
 
 #compile wayland
-rm $PREFIX/lib/libwayland*
+clear
+TITLE "Compiling wayland... (5/8)"
+echo ""
+
+RM_SILENT $PREFIX/lib/libwayland*
 
 cd $TMP_FOLDER/wayland
 
@@ -215,10 +260,14 @@ mkdir b
 cd b
 
 meson -Dprefix=$PREFIX -Dtests=false -Ddocumentation=false -Dbuildtype=release ..
-ninja install
+ninja install -d stats
 
 #compile wayland-protocols
-rm /data/data/com.termux/files/usr/lib/pkgconfig/wayland-protocols.pc
+clear
+TITLE "Compiling wayland-protocols... (6/8)"
+echo ""
+
+RM_SILENT /data/data/com.termux/files/usr/lib/pkgconfig/wayland-protocols.pc
 
 cd $TMP_FOLDER/wayland-protocols
 
@@ -226,21 +275,48 @@ mkdir b
 cd b
 
 meson -Dprefix=$PREFIX -Dtests=false -Dbuildtype=release ..
-ninja install
-
+ninja install -d stats
 
 #compile libsha1
+clear
+TITLE "Compiling libsha1... (7/8)"
+echo ""
+
 cd $TMP_FOLDER/libsha1
 
-rm $PREFIX/lib/libsha1*
+RM_SILENT $PREFIX/lib/libsha1*
 
 ./autogen.sh --prefix=$PREFIX
-make -j8 install
+make -s -j8 install
 
 #compile Xwayland
+clear
+TITLE "Compiling & Patching xserver... (8/8)"
+echo ""
+
 cd $TMP_FOLDER/xserver
-[ ! -f $XSERVER_PATCH_FILE ] && DIE "XServer patch file not found! Try re-running the script..."
+[ ! -f $XSERVER_PATCH_FILE ] && {
+	DIE "xserver patch file not found! Try re-running the script..."
+}
 git apply $XSERVER_PATCH_FILE
 
-./autogen.sh --enable-mitshm --enable-xcsecurity --enable-xf86bigfont --enable-xwayland --enable-xorg --enable-xnest --enable-xvfb --disable-xwin --enable-xephyr --enable-kdrive --disable-devel-docs --disable-config-hal --disable-config-udev --disable-unit-tests --disable-selective-werror --disable-static --without-dtrace --disable-glamor --enable-glx --with-sha1=libsha1 --with-pic --prefix=$PREFIX
-make -j8 install LDFLAGS='-fuse-ld=lld /data/data/com.termux/files/usr/lib/libandroid-shmem.a -llog'
+[[ "$USE_XF86BF" = "enable" || "$USE_XF86BF" = "fix" ]] && {
+	./autogen.sh --enable-mitshm --enable-xcsecurity --enable-xf86bigfont --enable-xwayland --enable-xorg --enable-xnest --enable-xvfb --disable-xwin --enable-xephyr --enable-kdrive --disable-devel-docs --disable-config-hal --disable-config-udev --disable-unit-tests --disable-selective-werror --disable-static --without-dtrace --disable-glamor --enable-glx --with-sha1=libsha1 --with-pic --prefix=$PREFIX
+} || {
+	./autogen.sh --enable-mitshm --enable-xcsecurity --disable-xf86bigfont --enable-xwayland --enable-xorg --enable-xnest --enable-xvfb --disable-xwin --enable-xephyr --enable-kdrive --disable-devel-docs --disable-config-hal --disable-config-udev --disable-unit-tests --disable-selective-werror --disable-static --without-dtrace --disable-glamor --enable-glx --with-sha1=libsha1 --with-pic --prefix=$PREFIX
+}
+
+[ "$USE_XF86BF" = "fix" ] && {
+	make -s -j8 install LDFLAGS='-fuse-ld=lld /data/data/com.termux/files/usr/lib/libandroid-shmem.a -llog' CPPFLAGS=-DSHMLBA=4096 # CHANGE THIS IF CRASHING OR SMTH
+} || {
+	make -s -j8 install LDFLAGS='-fuse-ld=lld /data/data/com.termux/files/usr/lib/libandroid-shmem.a -llog'
+}
+
+clear
+TITLE "DONE!"
+INFO "Build success!"
+
+INFO "Termux-X11 is recommended when using this!"
+
+INFO "Script signing off..."
+exit 0
